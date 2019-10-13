1e7d3462d15e978a922310dabd4c90d736ddc43c
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-10-13 13:33:58 +0300

    primitive win coditions working game

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // moveNum is a counter for move number, game checks for winner at move 6
-let moveNum = 0;
+let moveNum = 1;
 
 
 // selector for boxes
@@ -16,21 +16,60 @@ const $box9 = $("#box9");
 
 // this function changes the box to x or o based on madulo of moveNum
 const addPlay = function() {
-    // plays o
+    // plays O then remove listner for played box
     if (moveNum % 2 == 0) {
         $(this).text("O");
         $(this).addClass("o");
         $(this).off();
         moveNum += 1;
+        if (moveNum > 5) {
+            checkForWin()
+        }
         
     } else {
-        // plays x
+        // plays X then remove listner for played box
         $(this).text("X");
         $(this).addClass("x");
         $(this).off();
         moveNum += 1;
-
+        if (moveNum > 5) {
+            checkForWin()
+        }
     }
+}
+
+const checkForWin = function () {
+    // check rows
+    if (($($box1).text() == 'X' && $($box2).text()=="X" && $($box3).text()=="X") || ($($box4).text() == 'X' && $($box5).text()=="X" && $($box6).text()=="X") || ($($box7).text() == 'X' && $($box8).text()=="X" && $($box9).text()=="X")) {
+
+        alert("X wins")
+       
+    } else if (($($box1).text() == 'O' && $($box2).text()=="O" && $($box3).text()=="O") || ($($box4).text() == 'O' && $($box5).text()=="O" && $($box6).text()=="O") || ($($box7).text() == 'O' && $($box8).text()=="O" && $($box9).text()=="O")) {
+
+        alert("O wins")
+
+     // check columns   
+    } else if (($($box1).text() == 'X' && $($box4).text()=="X" && $($box7).text()=="X") || ($($box2).text() == 'X' && $($box5).text()=="X" && $($box8).text()=="X") || ($($box3).text() == 'X' && $($box6).text()=="X" && $($box9).text()=="X")) {
+
+        alert("X wins")
+        
+
+    } else if (($($box1).text() == 'O' && $($box4).text()=="O" && $($box7).text()=="O") || ($($box2).text() == 'O' && $($box5).text()=="O" && $($box8).text()=="O") || ($($box3).text() == 'O' && $($box6).text()=="O" && $($box9).text()=="O")) {
+        
+        alert("O wins")
+       
+    // check diagonal
+    } else if (($($box1).text() == 'X' && $($box5).text()=="X" && $($box9).text()=="X") || ($($box3).text() == 'X' && $($box5).text()=="X" && $($box7).text()=="X")) {
+
+        alert("X wins")
+
+    } else if (($($box1).text() == 'O' && $($box5).text()=="O" && $($box9).text()=="O") || ($($box3).text() == 'O' && $($box5).text()=="O" && $($box7).text()=="O")) {
+        
+        alert("O wins")
+    }
+
+    
+    
 }
 
 
